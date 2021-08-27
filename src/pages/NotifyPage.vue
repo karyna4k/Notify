7c6b95d47679e95e2399ebb9ae999e6b03ce0456
Karyna Kananovich <karyna.kananovich@gmail.com>
2021-08-27 20:25:28 +0300

    feat: add style to NotifyPage page

--- src/pages/NotifyPage.vue
+++ src/pages/NotifyPage.vue
@@ -2,7 +2,14 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-        <notify :messages="messages" />
+        <div class="notify">
+          <div class="notify__title">
+            <h1>Notify App</h1>
+          </div>
+          <div class="notify__content">
+            <notify :messages="messages" />
+          </div>
+        </div>
       </div>
     </section>
   </div>
@@ -27,3 +34,35 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  height: 90vh;
+}
+
+.notify {
+  max-width: 400px;
+  width: 100%;
+  margin: auto;
+  padding: 32px;
+  border-radius: 16px;
+  background-color: #fff;
+  box-shadow: 0 12px 22px 0 rgba(0,0,0, 0.1);
+
+  &__title {
+    h1 {
+      margin-bottom: 16px;
+      font-size: 24px;
+      font-weight: 600;
+    }
+  }
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-height: 240px;
+  }
+}
+</style>
